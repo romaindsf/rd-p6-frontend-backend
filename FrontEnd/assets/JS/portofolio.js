a510415bdf7f4a6f23c3e3e9f598db73c741aab4
Romain Da Silva Faria <romaindsf@hotmail.fr>
2023-12-23 13:58:29 +0100

    not working _break_ next idea written at the end

--- FrontEnd/assets/JS/portofolio.js
+++ FrontEnd/assets/JS/portofolio.js
@@ -1,20 +1,19 @@
 //Récupération des projets de l'architecte depuis l'API
     const responseWorks = await fetch("http://localhost:5678/api/works");
     const projects = await responseWorks.json();
-    console.log(projects);
     const responseCategories = await fetch("http://localhost:5678/api/categories");
     const categories = await responseCategories.json();
-    console.log(categories);
 
+//déclaration des varaibles globales
+// Récupération de l'élément du DOM qui accueillera les fiches
+const divGallery = document.querySelector(".gallery");
 //Afficher les travaux depuis le back-end
 
 function genererTravaux(projects) {
     for (let i = 0; i< projects.length; i++) {
-        // Récupération de l'élément du DOM qui accueillera les fiches
-        const divGallery = document.querySelector(".gallery");
+        
         // Création d’une balise dédiée à un projet
         const projetElement = document.createElement("figure");
-            //projetElement.dataset.id = projects[i].id
         //remplis <figure> du contenu dédié (img & figcaption)
         const imgElement = document.createElement("img");
         imgElement.src = projects[i].imageUrl;
@@ -40,11 +39,33 @@ for(let i = 1; i < btnCategories.length; i++) {
     btnCategories[i].dataset.id = categories[i - 1].id;
     //ajout event listener
     btnCategories[i].addEventListener("click", (event) => {
-        //si le data-id du bouton pressé a id différente des id des projets
-        projects.forEach(projet => {
-            if(event.target.dataset.id != projet.category.id) {
-                projet.style.display = "none";
+        const listProjet = document.querySelectorAll("figure")
+        console.log(listProjet)
+        //si le data-id du bouton pressé a une id différente des id des projets
+        listProjet.forEach(element => {
+            console.log(element.category.id)
+            if(event.target.dataset.id != element.category.id) {
+                console.log("hide!")
             }
         });
     })
 };
+
+/*
+projects.forEach(Element => {
+            if(event.target.dataset.id != Element.category.id) {
+                console.log(event.target.dataset.id)
+                console.log(projet[1].category.id)
+                //le projet est alors caché
+                projet.style.display = "none";
+            }
+        });
+for(let i = 0; i < projects.length; i++) {
+    console.log(projects[i].category.id);
+}
+
+prohcaine idée :
+rajouter une classe dans boucle génére à chaque figure correspondant à sa catégorie
+et utiliser cette nouvelle classe comme comparatif dans event.listener => if
+display non sur la <figure> en question
+*/
